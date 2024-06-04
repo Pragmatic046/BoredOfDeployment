ae1f24d9f47cde1cae3a8381fbdd50568341e7d8
Abdul Ahad Khan <aakabdulahad7@gmail.com>
2024-06-04 19:14:55 +0500

    boooring

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>At the beginning of this react and deployment it's so boring.</h1>
+     <h2>Really bored of it especially at gitHub and Firebase deployment</h2>
+    </>
+  )
+}
+
+export default App
